9e415149de519aa9204432bd4c120dd20ce13592
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-20 18:55:52 +0300

    added class worker

---
+++ app/Worker.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+// Все "переменные" класса - поля
+// Все "функции" класса - методы
+class Worker {
+    // Каждый класс должен выполнять какое-то одно конкретное действия.
+    // Второе действие - второй класс, в один всё пихать не надо.
+     public string $name;
+     public int $age;
+     public array $hours;
+
+     public function work() {
+         // work
+     }
+
+}
