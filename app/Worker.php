cc995a41631e61dbb41ad6566026ddbd90217612
misha366 <misha.moskalenko.jv@gmail.com>
2025-01-21 18:05:25 +0300

    implemented constructor in worker class

--- app/Worker.php
+++ app/Worker.php
@@ -14,6 +14,12 @@ class Worker {
      public int $age;
      public array $hours;
 
+    public function __construct(string $name, int $age, array $hours) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->hours = $hours;
+    }
+
      public function work() {
          echoHtmlLine("working...");
      }
--- public/index.php
+++ public/index.php
@@ -3,5 +3,6 @@ declare(strict_types=1);
 
 require_once "../vendor/autoload.php";
 
-$worker = new \App\Worker();
-$worker->work();
+$worker = new \App\Worker("John", 27, [2, 6, 10]);
+//$worker->work();
+var_dump($worker);
